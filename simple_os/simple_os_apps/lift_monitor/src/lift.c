92a968948b309a38d4eab93a41a41ba34b73fdca
Johan Kllstrm <johka546@student.liu.se>
2013-11-22 11:28:48 +0100

    LiftMonitor: mera

--- simple_os/simple_os_apps/lift_monitor/src/lift.c
+++ simple_os/simple_os_apps/lift_monitor/src/lift.c
@@ -1,57 +1,244 @@
 #include "lift.h"
 
-/* definition of monitor data type for lift */
+/* Simple_OS include */ 
+#include <simple_os.h>
 
-typedef struct
+/* drawing module */ 
+#include "draw.h"
+
+/* standard includes */ 
+#include <stdlib.h>
+#include <stdio.h>
+
+/* panic function, to be called when fatal errors occur */ 
+static void lift_panic(const char message[])
 {
-    /* the floor where the lift is positioned */ 
+    printf("LIFT_PANIC!!! "); 
+    printf("%s", message); 
+    printf("\n"); 
+    exit(0); 
+}
+
+/* --- monitor data type for lift and operations for create and delete START --- */
+
+/* lift_create: creates and initialises a variable of type lift_type */
+lift_type lift_create(void) 
+{
+    /* the lift to be initialised */
+    lift_type lift;
+
+    /* floor counter */ 
     int floor; 
 
-    /* a flag to indicate if the lift is moving */ 
-    int moving; 
+    /* loop counter */
+    int i;
 
-    /* variable to indicate if the lift is travelling in the up 
-       direction, which is defined as the direction where the 
-       floor number is increasing */
-    int up;
+    /* allocate memory */
+    lift = (lift_type) malloc(sizeof(lift_data_type));
 
-    /* persons on each floor waiting to enter */ 
-    person_data_type persons_to_enter[N_FLOORS][MAX_N_PERSONS];
+    /* initialise variables */
 
-    /* passengers in the lift */
-    person_data_type passengers_in_lift[MAX_N_PASSENGERS];
+    /* initialise floor */
+    lift->floor = 0; 
+    
+    /* set direction of lift travel to up */
+    lift->up = 1;
 
-    /* semaphore for mutual exclusion */
-    si_semaphore mutex; 
+    /* the lift is not moving */ 
+    lift->moving = 0; 
 
-    /* condition variable, to indicate that something has happend */ 
-    si_condvar change; 
+    /* initialise person information */
+    for (floor = 0; floor < N_FLOORS; floor++)
+    {
+        for (i = 0; i < MAX_N_PERSONS; i++)
+        {
+            lift->persons_to_enter[floor][i].id = NO_ID; 
+            lift->persons_to_enter[floor][i].to_floor = NO_FLOOR; 
+        }
+    }
 
-} lift_data_type;
+    /* initialise passenger information */
+    for (i = 0; i < MAX_N_PASSENGERS; i++) 
+    {
+        lift->passengers_in_lift[i].id = NO_ID; 
+        lift->passengers_in_lift[i].to_floor = NO_FLOOR; 
+    }
 
-typedef lift_data_type* lift_type;
+    /* initialise semaphore and event variable */
+    si_sem_init(&lift->mutex, 1); 
+    si_cv_init(&lift->change, &lift->mutex); 
 
-/* data structure for person information */ 
-typedef struct
-{
-    /* identity */ 
-    int id; 
-    /* destination floor */ 
-    int to_floor; 
-} person_data_type; 
-
-/* random_level: computes a randomly chosen level */
-int random_level(void)
-{
-    /* return random number between 0 and N_FLOORS-1 */
-    return rand() % N_FLOORS;
+    return lift;
 }
 
-void init_lift(void)
+/* lift_delete: deallocates memory for lift */
+void lift_delete(lift_type lift)
 {
-    /* use this statement for different random sequences at 
-    different program executions, e.g. when the final version 
-    of the program is ready */
-    srand(time(NULL));
+    free(lift);
 }
+
+
+/* --- monitor data type for lift and operations for create and delete END --- */
+
+
+/* --- functions related to lift task START --- */
+
+/* MONITOR function lift_next_floor: computes the floor to which the lift 
+   shall travel. The parameter *change_direction indicates if the direction 
+   shall be changed */
+void lift_next_floor(lift_type lift, int *next_floor, int *change_direction)
+{
+
+}
+
+void lift_move(lift_type lift, int next_floor, int change_direction)
+{
+    /* reserve lift */ 
+    si_sem_wait(&lift->mutex); 
+
+    /* the lift is moving */ 
+    lift->moving = 1; 
+        
+    /* release lift */ 
+    si_sem_signal(&lift->mutex); 
+        
+    /* it takes two seconds to move to the next floor */ 
+    si_wait_n_ms(2000); 
+        
+    /* reserve lift */ 
+    si_sem_wait(&lift->mutex); 
+        
+    /* the lift is not moving */ 
+    lift->moving = 0; 
+
+    /* the lift has arrived at next_floor */ 
+    lift->floor = next_floor; 
+
+    /* check if direction shall be changed */ 
+    if (change_direction)
+    {
+        lift->up = !lift->up; 
+    }
+
+    /* draw, since a change has occurred */ 
+    draw_lift(lift); 
+
+    /* release lift */ 
+    si_sem_signal(&lift->mutex); 
+}
+
+/* this function is used also by the person tasks */ 
+static int n_passengers_in_lift(lift_type lift)
+{
+    int n_passengers = 0; 
+    int i; 
+        
+    for (i = 0; i < MAX_N_PASSENGERS; i++)
+    {
+        if (lift->passengers_in_lift[i].id != NO_ID)
+        {
+            n_passengers++; 
+        }
+    }
+    return n_passengers; 
+}
+
+/* MONITOR function lift_has_arrived: shall be called by the lift task
+   when the lift has arrived at the next floor. This function indicates
+   to other tasks that the lift has arrived, and then waits until the lift
+   shall move again. */
+void lift_has_arrived(lift_type lift)
+{
+
+}
+
+/* --- functions related to lift task END --- */
+
+
+/* --- functions related to person task START --- */
+
+/* passenger_wait_for_lift: returns non-zero if the passenger shall
+   wait for the lift, otherwise returns zero */
+static int passenger_wait_for_lift(lift_type lift, int wait_floor)
+{
+    int waiting_ready =
+        /* the lift is not moving */ 
+        !lift->moving && 
+        /* and the lift is at wait_floor */ 
+        lift->floor == wait_floor && 
+        /* and the lift is not full */ 
+        n_passengers_in_lift(lift) < MAX_N_PASSENGERS; 
+
+    return !waiting_ready;
+}
+
+/* enter_floor: makes a person with id id stand at floor floor */ 
+static void enter_floor(
+    lift_type lift, int id, int floor)
+{
+    int i; 
+    int floor_index; 
+    int found; 
+
+    /* stand at floor */ 
+    found = 0; 
+    for (i = 0; i < MAX_N_PERSONS && !found; i++)
+    {
+        if (lift->persons_to_enter[floor][i].id == NO_ID)
+        {
+            found = 1; 
+            floor_index = i; 
+        }
+    }
+        
+    if (!found)
+    {
+        lift_panic("cannot enter floor"); 
+    }
+
+    /* enter floor at index floor_index */ 
+    lift->persons_to_enter[floor][floor_index].id = id; 
+    lift->persons_to_enter[floor][floor_index].to_floor = NO_FLOOR; 
+}
+
+/* leave_floor: makes a person with id id at enter_floor leave 
+   enter_floor */ 
+static void leave_floor(
+    lift_type lift, int id, int enter_floor)
+
+/* fig_end lift_c_prot */ 
+{
+    int i; 
+    int floor_index; 
+    int found; 
+
+    /* leave the floor */
+    found = 0; 
+    for (i = 0; i < MAX_N_PERSONS && !found; i++)
+    {
+        if (lift->persons_to_enter[enter_floor][i].id == id)
+        {
+            found = 1; 
+            floor_index = i; 
+        }
+    }
+        
+    if (!found)
+    {
+        lift_panic("cannot leave floor"); 
+    }
+
+    /* leave floor at index floor_index */ 
+    lift->persons_to_enter[enter_floor][floor_index].id = NO_ID; 
+    lift->persons_to_enter[enter_floor][floor_index].to_floor = NO_FLOOR; 
+}
+
+/* MONITOR function lift_travel: performs a journey with the lift
+   starting at from_floor, and ending at to_floor */ 
+void lift_travel(lift_type lift, int id, int from_floor, int to_floor)
+{
+
+}
+
+/* --- functions related to person task END --- */
 
--- simple_os/simple_os_apps/lift_monitor/src/lift.h
+++ simple_os/simple_os_apps/lift_monitor/src/lift.h
@@ -1,3 +1,74 @@
+#ifndef LIFT_H
+#define LIFT_H
+
+#include <simple_os.h>
+
+/* fig_begin lift_h_defs */ 
+/* size of building */ 
+#define N_FLOORS 5
+
+/* maximum number of persons in the lift system */ 
+#define MAX_N_PERSONS 10
+
+/* maximum number of passengers in lift */ 
+#define MAX_N_PASSENGERS 5
+/* fig_end lift_h_defs */ 
+
+/* fig_begin person_data_type */ 
+/* data structure for person information */ 
+typedef struct
+{
+    /* identity */ 
+    int id; 
+    /* destination floor */ 
+    int to_floor; 
+} person_data_type; 
+/* fig_end person_data_type */ 
+
+/* special numbers, to define no identity and no destination */ 
+#define NO_ID -1
+#define NO_FLOOR -1
+
+/* fig_begin lift_mon_type */ 
+/* definition of monitor data type for lift */
+
+typedef struct
+{
+    /* the floor where the lift is positioned */ 
+    int floor; 
+
+    /* a flag to indicate if the lift is moving */ 
+    int moving; 
+
+    /* variable to indicate if the lift is travelling in the up 
+       direction, which is defined as the direction where the 
+       floor number is increasing */
+    int up;
+
+    /* persons on each floor waiting to enter */ 
+    person_data_type persons_to_enter[N_FLOORS][MAX_N_PERSONS];
+
+    /* passengers in the lift */
+    person_data_type passengers_in_lift[MAX_N_PASSENGERS];
+
+    /* semaphore for mutual exclusion */
+    si_semaphore mutex; 
+
+    /* condition variable, to indicate that something has happend */ 
+    si_condvar change; 
+
+} lift_data_type;
+
+typedef lift_data_type* lift_type;
+/* fig_end lift_mon_type */ 
+
+/* lift_create: creates and initialises a variable of type lift_type */
+lift_type lift_create(void); 
+
+/* lift_delete: deallocates memory for lift */
+void lift_delete(lift_type lift); 
+
+/* fig_begin mon_functions */ 
 /* MONITOR function lift_next_floor: computes the floor to which 
    the lift shall travel. The parameter *change_direction 
    indicates if the direction shall be changed */
@@ -22,16 +93,22 @@ void lift_has_arrived(lift_type lift);
    at to_floor */ 
 void lift_travel(
     lift_type lift, int id, int from_floor, int to_floor);
+/* fig_end mon_functions */ 
 
 int random_level(void);
 
 void init_lift(void);
 
-/* size of building */ 
-#define N_FLOORS 5
+/* The shall be one task for each person. All person tasks shall be implemented by the same C function, called passenger_task. */
+void passenger_task(void);
 
-/* maximum number of persons in the lift system */ 
-#define MAX_N_PERSONS 10
+/* There shall be one task, called lift_task, for the lift.  */
+void lift_task(void);
 
-/* maximum number of passengers in lift */ 
-#define MAX_N_PASSENGERS 5
+/*
+There shall be one task, called user_task, which receives commands from the graphical user interface. This task shall create new person tasks. A new person task shall be created when the text string new is received. The task user_task shall also contain functionality for termination of the program. The program shall be terminated when the text string exit is received. 
+*/
+void user_task(void);
+
+
+#endif
